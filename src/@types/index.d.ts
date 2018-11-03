a7e94f4bcde9c3636ddb59f06001bceeb2fefbe2
guzhongren <guzhongren@live.cn>
2018-11-03 20:45:14 +0800

    Update index.d.ts

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,6 +3,7 @@ declare var require: {
   (paths: string[], callback: (...modules: any[]) => void): void;
   ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
 };
+
 declare module "*.gif";
 declare module "*.jpg";
 declare module "*.jpeg";
