37d39f7110fe829975d1644a5374f0aa28ce7dc2
guzhongren <guzhongren@live.cn>
2018-12-06 16:50:45 +0800

    test(cd): 测试

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const {
   GenerateSW
 } = require('workbox-webpack-plugin')
 // const tsImportPluginFactory = require('ts-import-plugin')
-const publicUrl = isProduction ? './PlatformWeb/' : '/'
+const publicUrl = isProduction ? '/PlatformWeb/' : '/'
 module.exports = {
   mode: isProduction ? 'production' : 'development',
   entry: {
