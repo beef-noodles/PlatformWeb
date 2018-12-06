bd1bf6f6bf374dd96e50eaf7551fec503cfa26db
guzhongren <guzhongren@live.cn>
2018-12-06 16:32:48 +0800

    test(cd): 测试

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const {
   GenerateSW
 } = require('workbox-webpack-plugin')
 // const tsImportPluginFactory = require('ts-import-plugin')
-const publicUrl = '/beef-noodles.github.io'
+const publicUrl = !isProduction ? 'https://beef-noodles.github.io/PlatformWeb' : '/'
 module.exports = {
   mode: isProduction ? 'production' : 'development',
   entry: {
