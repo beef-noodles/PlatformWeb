ce9d29563054b1d9902e574c810cf544fa31b8dc
guzhongren <guzhongren@live.cn>
2018-12-07 01:44:10 +0000

    Update docs

---
+++ precache-manifest.f178bdf8cbd5732a56a96bc622d72b36.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2fa34de139501e77481e947df04d891",
+    "url": "/PlatformWeb/static/fonts/f2fa34.woff"
+  },
+  {
+    "revision": "29b2db8843bebddb9a98",
+    "url": "/PlatformWeb/static/style/vendors.css"
+  },
+  {
+    "revision": "4c40e950d3719c21f75c2dc7a9312999",
+    "url": "/PlatformWeb/static/fonts/4c40e9.ttf"
+  },
+  {
+    "revision": "c48393130998de6a5438",
+    "url": "/PlatformWeb/static/style/app.css"
+  },
+  {
+    "revision": "1b35b521c0082d56b9ac413ddb0660ef",
+    "url": "/PlatformWeb/static/fonts/1b35b5.woff"
+  },
+  {
+    "revision": "125de81ae7284e588313",
+    "url": "/PlatformWeb/static/js/runtime_bundle.js"
+  },
+  {
+    "revision": "b6b5f9f63c8edb9d680a",
+    "url": "/PlatformWeb/static/style/3.css"
+  },
+  {
+    "revision": "20dcb93cf2dfddd5030f",
+    "url": "/PlatformWeb/static/style/4.css"
+  },
+  {
+    "revision": "29b2db8843bebddb9a98",
+    "url": "/PlatformWeb/static/js/vendors_bundle.js"
+  },
+  {
+    "revision": "b365eab71ff92ff7704f",
+    "url": "/PlatformWeb/static/style/5.css"
+  },
+  {
+    "revision": "22e8085fa7f504f20955",
+    "url": "/PlatformWeb/static/style/6.css"
+  },
+  {
+    "revision": "2adfe0592c9854f92481120aed147bb8",
+    "url": "/PlatformWeb/static/images/2adfe0.png"
+  },
+  {
+    "revision": "b365eab71ff92ff7704f",
+    "url": "/PlatformWeb/static/js/5_bundle.js"
+  },
+  {
+    "revision": "b8ac342d7d6e822b3cd4",
+    "url": "/PlatformWeb/static/js/7_bundle.js"
+  },
+  {
+    "revision": "aa7e8abe16e2262bc55d",
+    "url": "/PlatformWeb/static/js/8_bundle.js"
+  },
+  {
+    "revision": "22e8085fa7f504f20955",
+    "url": "/PlatformWeb/static/js/6_bundle.js"
+  },
+  {
+    "revision": "c48393130998de6a5438",
+    "url": "/PlatformWeb/static/js/app_bundle.js"
+  },
+  {
+    "revision": "b84aec9784599c3517f2f10ceb5d997e",
+    "url": "/PlatformWeb/static/fonts/b84aec.woff"
+  },
+  {
+    "revision": "20dcb93cf2dfddd5030f",
+    "url": "/PlatformWeb/static/js/4_bundle.js"
+  },
+  {
+    "revision": "a009f7f796cff0dfdb8eb5aa135b4cae",
+    "url": "/PlatformWeb/static/images/a009f7.png"
+  },
+  {
+    "revision": "b6b5f9f63c8edb9d680a",
+    "url": "/PlatformWeb/static/js/3_bundle.js"
+  },
+  {
+    "revision": "358fcb5b8cd5b2abf451fc36e0480e74",
+    "url": "/PlatformWeb/static/images/358fcb.png"
+  },
+  {
+    "revision": "92acc6829cf612e62ecf2be54e9344f0",
+    "url": "/PlatformWeb/static/fonts/92acc6.ttf"
+  },
+  {
+    "revision": "657910a808ed7e42fcc4327f04c586b8",
+    "url": "/PlatformWeb/static/images/657910.png"
+  },
+  {
+    "revision": "7b261cf1d84d3cc3978a2db328c9732d",
+    "url": "/PlatformWeb/static/images/7b261c.jpg"
+  },
+  {
+    "revision": "bdbbe7f8715d4508312fa4c3b81999f8",
+    "url": "/PlatformWeb/static/fonts/bdbbe7.ttf"
+  },
+  {
+    "revision": "22ffa1b3964afadd3f8f4cd1b39d9a02",
+    "url": "/PlatformWeb/static/images/22ffa1.png"
+  },
+  {
+    "revision": "ca5d4588dad9c32f49e895e7f19479df",
+    "url": "/PlatformWeb/static/images/ca5d45.svg"
+  },
+  {
+    "revision": "8c9b05a6db51b83bbeb48a623489b2aa",
+    "url": "/PlatformWeb/static/fonts/8c9b05.eot"
+  },
+  {
+    "revision": "62759b1cf27452e085eddbb8ab29ad2b",
+    "url": "/PlatformWeb/static/fonts/62759b.eot"
+  },
+  {
+    "revision": "207b668a35ae8000938fe3a11c7d9ac0",
+    "url": "/PlatformWeb/static/fonts/207b66.eot"
+  },
+  {
+    "revision": "657910a808ed7e42fcc4327f04c586b8",
+    "url": "/PlatformWeb/icon.png"
+  },
+  {
+    "revision": "b5f4be6421ade8fc5bdcde1839afd32f",
+    "url": "/PlatformWeb/favicon.ico"
+  },
+  {
+    "revision": "b0ebcf61d580987a8b3b075bd6aeff6a",
+    "url": "/PlatformWeb/config.js"
+  },
+  {
+    "revision": "9702e1c97185179cdfbeb7c0b4f3c698",
+    "url": "/PlatformWeb/./index.html"
+  }
+];
---
+++ static/js/8_bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1344:function(e,t,n){"use strict";n.r(t);var i=n(1);t.default=function(){return i.createElement("div",{style:{height:"1100px"}},"test")}}}]);
